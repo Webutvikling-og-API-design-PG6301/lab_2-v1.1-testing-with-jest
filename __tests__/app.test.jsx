e2f854dfe6854c6c7607ef505f95246ef833f131
StianOek <66445281+StianOek@users.noreply.github.com>
2022-01-27 08:07:42 +0100

    tests

--- __tests__/app.test.jsx
+++ __tests__/app.test.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import {render} from "react-dom";
-import {FrontPage} from "../App";
-import {MemoryRouter} from "react-router-dom";
+import React from "react";
+import { render } from "react-dom";
+import { FrontPage } from "../App";
+import { MemoryRouter } from "react-router-dom";
 
 describe("Frontpage", () => {
+  it("should return true", () => {
+    expect(true).toBeTruthy();
+  });
 
-    it("should make all component to snapshot", () => {
-        const element = document.createElement("div");
-        render(<MemoryRouter><FrontPage /></MemoryRouter>, element)
-        expect(element.innerHTML).toMatchSnapshot()
+  it("should make all component to snapshot", () => {
+    const element = document.createElement("div");
+    render(
+      <MemoryRouter>
+        <FrontPage />
+      </MemoryRouter>,
+      element
+    );
+    expect(element.innerHTML).toMatchSnapshot();
+  });
 
-    })
-
-    it("should show answer status", () => {
-        const element = document.createElement("div");
-        render(<MemoryRouter>
-            <FrontPage correctAnswer={3} questionsAnswered={11} />
-        </MemoryRouter>, element)
-        expect(element.querySelector("[data-testid=status]").textContent)
-    })
-
-
-})
+  it("should show answer status", () => {
+    const element = document.createElement("div");
+    render(
+      <MemoryRouter>
+        <FrontPage correctAnswer={3} questionsAnswered={11} />
+      </MemoryRouter>,
+      element
+    );
+    expect(element.querySelector("[data-testid=status]").textContent);
+  });
+});
